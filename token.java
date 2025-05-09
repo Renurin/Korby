6ae8bba721076eceebbd32f4c4339c63e643bdd8
Renurin <rcamaradeoliveirafarias@gmail.com>
2025-05-09 15:42:21 -0300

    defining token and tokenType

---
+++ token.java
@@ -0,0 +1,17 @@
+class token {
+    final tokenType type;
+    final String lexemme;
+    final Object literal;
+    final int line;
+
+    token(tokenType type, String lexemme, Object literal, int line){
+        this.type = type;
+        this.lexemme = lexemme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString(){
+        return type + " " + lexemme+ " " + literal; 
+    }
+}
